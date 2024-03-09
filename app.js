4d4e8b980f581f6a52cfcec3be7b047603f7b364
muhammadyasa <muhammadyasa307@gmail.com>
2024-03-09 13:42:58 +0500

    add

---
+++ app.js
@@ -0,0 +1,56 @@
+// chap 1
+
+//alert("Error! Please enter a valid Password"); 
+
+// alert("Welcome to JS Land...\n Happy cooding!"); 
+
+// alert("Welcome to JS Land...\n "); 
+
+// alert("Happy cooding!"); 
+
+// chap 2
+
+//q1 
+//var username;
+
+//q2
+//var myName = "Muhammad Yasa";
+
+//q3
+// var message;
+// message = "Hello World";
+// alert(message);
+
+//q4
+// var studentName = "Muhammad Yasa";
+// var studentAge = 20;
+// var studentGrade = "A";
+// var studentCity = "Krachi";
+
+// alert("Student Name: " + studentName + "\nStudent Age: " + studentAge + "\nStudent Grade: " + studentGrade + "\nStudent City: " + studentCity);
+
+// q5
+// var pizza = "Pizza\nPizz\nPiz\nPi\nP"
+// alert(pizza)
+
+// q6
+// var email = "My Email Address Is muhammayasa307@gmail.com"
+// alert(email)
+
+//q7
+// var message;
+// message = "I am tryin to learn from a book A smater way to learn java script !";
+// alert(message);
+
+// chap 3
+
+// q1
+//var age 
+//age = "My age is 19 years old."
+//alert(age);
+
+//q3
+// var birthYear 
+// birthYear = "My birth year is 2004"
+// document.write(birthYear)
+
